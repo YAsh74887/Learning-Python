626d63239d9174e2d4647299e26d10a62ed12d27
YAsh74887 <91481198+YAsh74887@users.noreply.github.com>
2022-08-09 20:01:34 +0530

    Add files via upload

---
+++ Caesar_cipher.py
@@ -0,0 +1,56 @@
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',]
+print(logo)   
+    
+def encode (plain_text,shift_amount):
+  cipher = " "
+  for letters in plain_text:
+    position = alphabet.index(letters)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    cipher += new_letter
+  print(cipher)
+  
+  
+  
+def decode(encrypt,shift_amount):
+  decode = " "
+  for letters in encrypt:
+    position = alphabet.index(letters)
+    new_position = position - shift_amount
+    new_letters = alphabet[new_position]
+    decode += new_letters
+  print(decode)   
+    
+   
+fun = input("enter encode or decode \n")    
+text=input("Input text: \n")
+shift=int(input("Input Shift no: \n"))
+
+shift = shift % 26 
+
+
+if fun == "encode":
+  encode(plain_text=text, shift_amount=shift) 
+  
+if fun =="decode":
+  decode(encrypt=text, shift_amount=shift)     
+      
